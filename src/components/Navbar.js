3de4fa1577edf9ec96c6d7dceb7439a44ca3a999
peterle0604 <petervole064@gmail.com>
2024-02-21 22:38:56 -0800

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "../assets/SCFLOGO.png";
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
+
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks)
+  };
+
   return (
     <div className='navbar'>
-      <div className='leftSide'>
+      <div className='leftSide' id={openLinks ? "open" : "close"}>
         <img src={Logo} />
+        <div className='hiddenLinks'>
+          <Link to="/"></Link>
+          <Link to="/About Us">About Us</Link>
+          <Link to="/GetInvolved">Get Involved</Link>
+          <Link to="/Volunteer">Volunteer</Link>
+          <Link to="/Donate">Contact</Link>      
+        </div>
       </div>
       <div className='rightSide'>
-        <Link to="/">Home</Link>
-        <Link to="/Volunteer">Volunteer</Link>
-        <Link to="/GetInvolved">Get Involved</Link>
-        <Link to="/Fridges">Fridges</Link>
+        <Link to="/"></Link>
         <Link to="/About Us">About Us</Link>
-        <Link to="/Donate">Donate</Link>
-        <button>
+        <Link to="/GetInvolved">Get Involved</Link>
+        <Link to="/Volunteer">Volunteer</Link>
+        <Link to="/Donate">Contact</Link>      
+        {/* <Link to="/Donate">Donate</Link> */}
+        <button onClick={toggleNavbar}>
         <ReorderIcon />
         </button>
       </div>
